44a24dcdf26b69a9e62e61dc7d313b2c5c418eef
sophea pheak <sopheapheak9@gmail.com>
2023-03-17 23:34:12 +0700

    version 1

---
+++ day 2 ex2 (hello).py
@@ -0,0 +1,4 @@
+welcome_string= "Hello"
+name =input("Please Enter your name : ")
+print ("---------------------------------------------")
+print (welcome_string +" "+  name + "!")
---
+++ day 2ex5.py
@@ -0,0 +1,4 @@
+n=int(input("please enter n value : "))
+sum=(n + 1)*n/2
+print("-------------------------------------")
+print("sum = n(n+1)/2  : %.2f"%sum)
